214dea035f088805b6562eeaa3bb10f67310ef6c
Alexis Poaquiza <106999645+AlexiP18@users.noreply.github.com>
2025-08-10 03:23:54 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+// Create web server
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from the comment web server!\n');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
